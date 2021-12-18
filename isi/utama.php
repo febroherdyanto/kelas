44551043d6b3c0dfb83d7374996988836f80bbcd
Febro Herdyanto <febroherdyanto@outlook.com>
2021-12-18 20:48:35 +0700

    Repair Session

--- isi/utama.php
+++ isi/utama.php
@@ -2,7 +2,7 @@
             <div class="col-md-12 grid-margin">
               <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h3 class="font-weight-bold">Selamat Datang <?php echo $_SESSION['nama_lengkap']." sebagai ".$_SESSION['level']; ?> di Sistem Informasi Kelas TI.20.B.1</h3>
+                  <h3 class="font-weight-bold">Selamat Datang <?php echo $_SESSION['nama_lengkap']." - ".$_SESSION['level']; ?> di Sistem Informasi Kelas TI.20.B.1</h3>
                 </div>
               </div>
             </div>
