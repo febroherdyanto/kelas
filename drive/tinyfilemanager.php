c35224b44457554e969c37c8a1a0f6effa344427
Febro Herdyanto <febroherdyanto@outlook.com>
2022-03-26 19:22:36 +0700

    Repair Downloading Task Files

--- drive/tinyfilemanager.php
+++ drive/tinyfilemanager.php
@@ -27,7 +27,7 @@ $use_auth = false;
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
     'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345 pass
 );
 
 // Readonly users
